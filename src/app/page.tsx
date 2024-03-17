b9d0493bc9abad944c762795141df9dd6b79c654
TheForeverOptimist <sean@elevate.app>
2024-03-17 09:47:15 -0700

    added MWW (max width wrapper) to app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <div></div>
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div></div>
+
+      </MaxWidthWrapper>
+    </>
   );
 }
