1b18e0a17e12c5592a03b9dfbe183c9ac82df2b3
TheForeverOptimist <sean@elevate.app>
2024-03-17 18:06:13 -0700

    updated page background, layout, globals, icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,47 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { SettingsIcon } from "lucide-react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowRight, SettingsIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <>
-      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-        <header className="flex justify-between fixed w-full top-0 text-white ">
-          <Image alt='Logo' src="https://i.imgur.com/MCHWJZS.png" height={50} width={50} className="object-contain" />
+    <main className="bg-black h-screen overscroll-y-auto">
+      <header className="flex justify-between fixed w-full top-0 text-white ">
+        <Image
+          alt="Logo"
+          src="https://i.imgur.com/MCHWJZS.png"
+          height={50}
+          width={50}
+          className="object-contain"
+        />
 
-          <SettingsIcon size={40} className="p-2 m-2 rounded-full cursor-pointer bg-blue-500 text-black transition-all ease-in-out duration-150 hover:bg-blue-700 hover:text-white" />
-        </header>
+        <SettingsIcon
+          size={40}
+          className="p-2 m-2 rounded-full cursor-pointer bg-blue-500 text-black transition-all ease-in-out duration-150 hover:bg-blue-700 hover:text-white"
+        />
+      </header>
 
-        
+      <form className="flex flex-col bg-black">
+        <div className="flex-1 bg-gradient-to-b from-blue-500 to-black">
+          <p>Hello</p>
+          {/* Messages */}
+          
+          </div>
+        {/* Hidden Fields */}
+        <input type="file" />
+        <button type="submit" hidden>
+          Submit Audio
+        </button>
+        <div className="fixed bottom-0 w-full overflow-hidden bg-black rounded-t-3xl">
+          {/* Recorder */}
+          <div>
+          {/* Voice Synthesizer -- output for Assistant Voice */}
 
-      </MaxWidthWrapper>
-    </>
+          </div>
+        </div>
+      </form>
+    </main>
   );
 }
