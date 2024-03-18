059d407d0f4433daedbd7b8934b7d79b6c9deaf7
TheForeverOptimist <sean@elevate.app>
2024-03-17 20:25:59 -0700

    added recorder component, recording logic, audiostreaming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
+"use client"
+
+
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Messages from "@/components/Messages";
+import Recorder, { mimeType } from "@/components/Recorder";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowRight, SettingsIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { useRef } from "react";
 
 export default function Home() {
+  const fileRef = useRef<HTMLInputElement | null>(null)
+  const submitButtonRef = useRef<HTMLButtonElement | null>(null)
+
+  const uploadAudio = (blob: Blob) => {
+    const url = URL.createObjectURL(blob);
+
+    const file = new File([blob], 'audio.webm', {type: mimeType})
+
+    //set the file as the value of the hidden file input field
+    if (fileRef.current) {
+       const dataTransfer = new DataTransfer();
+       dataTransfer.items.add(file);
+       fileRef.current.files = dataTransfer.files;
+
+       //simulate a click & submit the form
+       if (submitButtonRef.current) {
+        submitButtonRef.current.click();
+       }
+    }
+  }
+
   return (
     <main className="bg-black h-screen overscroll-y-auto">
       <header className="flex justify-between fixed w-full top-0 text-white ">
@@ -28,19 +53,15 @@ export default function Home() {
         <div className="flex-1 bg-gradient-to-b from-blue-500 to-black">
           <p>Hello</p>
           <Messages />
-          
-          </div>
+        </div>
         {/* Hidden Fields */}
-        <input type="file" />
-        <button type="submit" hidden>
+        <input type="file" name='audio' hidden ref={fileRef} />
+        <button type="submit" hidden ref={submitButtonRef}>
           Submit Audio
         </button>
         <div className="fixed bottom-0 w-full overflow-hidden bg-black rounded-t-3xl">
-          {/* Recorder */}
-          <div>
-          {/* Voice Synthesizer -- output for Assistant Voice */}
-
-          </div>
+          <Recorder uploadAudio={uploadAudio} />
+          <div>{/* Voice Synthesizer -- output for Assistant Voice */}</div>
         </div>
       </form>
     </main>
---
+++ src/components/Recorder.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import Image from "next/image";
+import activeIcon from "/img/active.gif";
+import nonActiveIcon from "/img/notactive.png";
+import { useEffect, useRef, useState } from "react";
+import { useFormStatus } from "react-dom";
+
+export const mimeType = "audio/webm";
+
+function Recorder({uploadAudio} :{uploadAudio: (blob:Blob) => void}) {
+    const mediaRecorder = useRef<MediaRecorder | null>(null)
+    const {pending} = useFormStatus();
+    const [permission, setPermission] = useState(false)
+    const [stream, setStream] = useState<MediaStream | null>(null)
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [audioChunks, setAudioChunks] = useState<Blob[]>([])
+
+
+
+
+
+    useEffect(() => {
+        getMicrophonePermission()
+
+    }, [])
+
+
+
+    //we are going to check for the media recorder
+    const getMicrophonePermission = async() => {
+        if ("MediaRecorder" in window) {
+          try {
+            const streamData = await navigator.mediaDevices.getUserMedia({
+              audio: true,
+              video: false,
+            });
+            setPermission(true);
+            setStream(streamData);
+          } catch (err: any) {
+            alert(err.message);
+          }
+        } else {
+          alert("The MediaRecorder API is not supported in your browser");
+        }
+    }
+
+    const startRecording = async () => {
+        if(stream === null || pending) return;
+
+        setRecordingStatus("recording");
+
+        // create a new media recorder instance using the stream
+        const media = new MediaRecorder(stream, {mimeType});
+        mediaRecorder.current = media;
+        mediaRecorder.current.start();
+
+        let localAudioChunks: Blob[] = [];
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === "undefined") return;
+            if (event.data.size === 0) return;
+            
+            localAudioChunks.push(event.data);
+        };
+        setAudioChunks(localAudioChunks);
+    }
+
+  return (
+    <div className="flex items-center justify-center text-white">
+      {!permission && (
+        <button onClick={getMicrophonePermission}>Get Microphone</button>
+      )}
+      {pending && (
+        <Image
+          src={activeIcon}
+          width={350}
+          height={350}
+          priority
+          alt="Recording"
+          className="assistant grayscale"
+        />
+      )}
+      {permission && recordingStatus === "inactive" && !pending && (
+        <Image
+          src={nonActiveIcon}
+          width={350}
+          height={350}
+          onClick={startRecording}
+          priority={true}
+          alt="Not Recording"
+          className="assistant cursor-pointer hover:scale-110 duration-150 transition-all ease-in-out"
+        />
+      )}
+    </div>
+  );
+}
+
+export default Recorder;
