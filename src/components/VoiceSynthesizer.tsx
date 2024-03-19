326a6abad90e02ae0bfe65dc4c0eb137e24ff230
TheForeverOptimist <sean@elevate.app>
2024-03-18 22:22:38 -0700

    added css and speechSynthesis to voice synthesizer

--- src/components/VoiceSynthesizer.tsx
+++ src/components/VoiceSynthesizer.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+
+
 type State = {
   sender: string;
   response: string | null | undefined;
@@ -11,11 +15,18 @@ function VoiceSynthesizer({
   displaySettings: boolean;
 }) {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center text-white">
       {displaySettings && (
         <>
           <div>
-            <p>Voice</p>
+            <p className="text-xs text-gray-500 p-2">Voice</p>
+                <select className="flex-1 bg-blue-500 text-white border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    {window.speechSynthesis.getVoices().map((voice) => (
+                        <option key={voice.name} value={voice.name}>
+                            {voice.name}
+                        </option>
+                    ))}
+                </select>
           </div>
         </>
       )}
