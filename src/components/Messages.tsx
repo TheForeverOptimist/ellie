7ad50280064be42ff37f16bc98dfae025f02e08d
TheForeverOptimist <sean@elevate.app>
2024-03-18 19:48:59 -0700

    added loading prompt indicator plus more message logic

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,6 @@
 import { Message } from "@/app/page"
 import { ChevronDownCircle } from "lucide-react"
+import LoadingPrompt from "./LoadingPrompt"
 
 
 interface MessageProps{
@@ -9,15 +10,17 @@ interface MessageProps{
 function Messages({messages}: MessageProps) {
   return (
     <div className={`flex flex-col min-h-screen p-5 pt-20 ${messages.length > 0 ? "pb-96" : "pb-52"}`}>
+
+      <LoadingPrompt />
       {!messages.length && (
         <div className="flex flex-col space-y-10 flex-1 items-center justify-end">
           <p className="text-white animate-pulse">Start a conversation</p>
           <ChevronDownCircle size={64} className="animate-bounce text-gray-500" />
         </div>
       )}
-      <div className="p-5">
+      <div className="p-5 space-y-5">
         {messages.map(message => (
-          <div key={message.id}>
+          <div key={message.id} className="space-y-5">
             {/* reciever */}
             <div className="pr-48">
               <p className="message bg-gray-800 rounded-bl-none">{message.response}</p>
